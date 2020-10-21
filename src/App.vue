91a30ab05f81c6f18692b759f652e8c1a7cbec61
mjsong <mjsong@yongsanzip.com>
2020-10-21 16:24:04 +0900

    로딩 스피닝 이미지 추가

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
+      <div class="loading-overlay" v-if="$store.state.is_show_loading">
+          <Circle2 size="80px" color="transparent" background="#00c7c9"></Circle2>
+      </div>
       <router-view :key="$route.fullPath"></router-view>
   </div>
 </template>
 
 <script>
+import {Circle2} from 'vue-loading-spinner'
+
 export default {
     name: 'app',
+    components: {
+        Circle2
+    },
     created() {
+        this.$store.dispatch('SAVE_IS_SHOW_LOADING', false);
         document.title = "액션제주";
     },
 }
--- src/store/index.js
+++ src/store/index.js
@@ -11,21 +11,32 @@ const vuexLocal = new VuexPersistence({
 
 export default new Vuex.Store({
     state:{
-        mb_id: null
+        mb_id: null,
+        is_show_loading: false,
     },
     getters:{
         GET_MB_ID(state) {
             return state.mb_id;
+        },
+        GET_IS_SHOW_LOADING(state) {
+            return state.is_show_loading;
         }
     },
     mutations:{
         SET_MB_ID(state, payload){
             state.mb_id = payload
+        },
+        SET_IS_SHOW_LOADING(state, payload){
+            state.is_show_loading = payload
         }
     },
     actions:{
         SAVE_MB_ID({commit}, payload) {
             commit('SET_MB_ID', payload);
+        },
+        SAVE_IS_SHOW_LOADING({commit}, payload) {
+            console.log("SAVE_IS_SHOW_LOADING, ", payload)
+            commit('SET_IS_SHOW_LOADING', payload);
         }
     },
 
