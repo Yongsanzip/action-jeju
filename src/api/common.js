a11ab14cf910a25a60828f6042437560818dc708
mjsong <mjsong@yongsanzip.com>
2020-12-28 10:52:14 +0900

    fix : common api request function

--- src/api/common.js
+++ src/api/common.js
@@ -1,35 +1,24 @@
 import axios from 'axios'
+
 const SETTINGS = {
+    BASIC_URL: 'http://220.90.200.93',
+    //BASIC_URL: 'http://www.actionjeju.com',
     DOMAIN: '/api'
 };
 
 export default {
     request(settings) {
         const GET_URL = settings.url;
-        // ajax로 시작하지않는 url은 api를 붙여준다.
-        /*if (!GET_URL.startsWith('/ajax')) {
-            settings.url = SETTINGS.DOMAIN + GET_URL;
-        }
-        if (process.env.NODE_ENV !== 'development'){
-            if (!GET_URL.startsWith('/ajax')) {
-                settings.url = 'http://www.actionjeju.com/societive' + SETTINGS.DOMAIN + GET_URL;
-            }else{
-                settings.url = 'http://www.actionjeju.com/societive' + GET_URL;
-            }
-        }*/
 
         if(GET_URL.startsWith('/bbs')) {
-            settings.url = 'http://www.actionjeju.com' + GET_URL;
+            settings.url = SETTINGS.BASIC_URL + GET_URL;
         }
         else if (!GET_URL.startsWith('/ajax')) {
-            settings.url = 'http://www.actionjeju.com/societive' + SETTINGS.DOMAIN + GET_URL;
+            settings.url = SETTINGS.BASIC_URL + '/societive' + SETTINGS.DOMAIN + GET_URL;
         }else{
-            settings.url = 'http://www.actionjeju.com/societive' + GET_URL;
+            settings.url = SETTINGS.BASIC_URL + '/societive' + GET_URL;
         }
 
-        // settings.url = 'http://www.actionjeju.com/societive' + SETTINGS.DOMAIN + GET_URL;
-
-        //console.log(settings)
         return axios(settings, settings.header != null? settings.header : null)
             .then(res => res)
             .catch(err => {
